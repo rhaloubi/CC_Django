ab4c2b49f1909be96c2fdbf397355407e6b966b6
rhaloubi <redahaloubi8@gmail.com>
2025-05-27 00:16:54 +0100

    remove the magic links

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -191,26 +191,11 @@ export default function LoginPage() {
                     <div className="absolute inset-0 flex items-center">
                       <span className="w-full border-t" />
                     </div>
-                    <div className="relative flex justify-center text-sm uppercase">
-                      <span className="bg-background px-2 text-muted-foreground">
-                        Or continue with
-                      </span>
-                    </div>
-                  </div>
-                  <div className="grid grid-cols-2 gap-4">
-                    <Button variant="outline" type="button" className="text-lg">
-                      <Github className="mr-2 h-4 w-4" />
-                      Github
-                    </Button>
-                    <Button variant="outline" type="button" className="text-lg">
-                      <Mail className="mr-2 h-4 w-4" />
-                      Google
-                    </Button>
                   </div>
                 </div>
                 <div className="flex flex-col space-y-2 text-center text-base">
                   <div className="text-muted-foreground">
-                    Don't have an account?{" "}
+                    {"Don't have an account?"} {" "}
                     <a href="/sign-up" className="text-primary underline underline-offset-4 hover:text-primary/90">
                       Sign up
                     </a>
